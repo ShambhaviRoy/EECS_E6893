60eee165c4d3c28e3d679bf554fa7bcfdc94e4eb
Shambhavi Roy <sr3767@columbia.edu>
2021-12-14 16:55:46 -0500

    update view.py

--- e6893_project/e6893_project/view.py
+++ e6893_project/e6893_project/view.py
@@ -66,7 +66,7 @@ def youtube(request):
     #   https://cloud.google.com/console
     # Please ensure that you have enabled the YouTube Data API for your project.
 
-    DEVELOPER_KEY = 'AIzaSyAovYYRq5qG_caQTp9lzQQWTsxLyFSAufY'
+    DEVELOPER_KEY = "INSERT DEVELOPER_KEY HERE"
     YOUTUBE_API_SERVICE_NAME = 'youtube'
     YOUTUBE_API_VERSION = 'v3'
 
